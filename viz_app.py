58c437bac61eaef218c6483a86dba02bcfed54b5
Harihara-sAI <hahasamo1008@gmail.com>
2024-02-18 20:54:11 +0530

    Update viz_app.py

--- viz_app.py
+++ viz_app.py
@@ -1,12 +1,109 @@
 import pandas as pd
 from statsbombpy import sb
 from mplsoccer import Pitch
+from mplsoccer import VerticalPitch
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 st.title("ShreeRamaJayam")
 
-data=sb.competitions()
+sb.competitions()
 
-st.dataframe(data)
+def get_league_data(i):
+    league_matches=sb.matches(competition_id=str(i),season_id='27')
+    return league_matches
 
+#premier_leagu_matches=get_league_data(2)
+#ligue_1=get_league_data(7)
+bundesliga=get_league_data(9)
+#la_liga=get_league_data(11)
+#serie_a=get_league_data(12)
+
+def get_team_matches(league,team):
+    away_matches=league[league["away_team"]==str(team)]
+    home_matches=league[league["home_team"]==str(team)]
+    matches=pd.concat([home_matches,away_matches],ignore_index=True)
+    return(matches)
+
+bvb_matches=get_team_matches(bundesliga, "Borussia Dortmund")
+
+bvb_matches.columns
+bvb_matches_list=bvb_matches[['match_id']]
+bvb_match_ids=bvb_matches_list['match_id'].to_list()
+
+m=bvb_match_ids[0]
+
+borussia_match=sb.events(match_id=m)
+
+borussia_match=borussia_match[['team','type','pass_type','location','pass_end_location','player','under_pressure','pass_outcome']].reset_index()
+borussia_match=borussia_match[(borussia_match['team']=='Borussia Dortmund') & (borussia_match['type']=='Pass') &(borussia_match['under_pressure']==True) ]
+
+borussia_match[['x_start', 'y_start']]=pd.DataFrame(borussia_match.location.to_list(), index=borussia_match.index)
+borussia_match[['x_end', 'y_end']]=pd.DataFrame(borussia_match.pass_end_location.to_list(), index=borussia_match.index)
+
+def make_pass_plot(pass_data):
+    pitch=Pitch(pitch_type='statsbomb')
+    fig, ax = pitch.draw(figsize=(15,8))
+    success=pd.isnull(pass_data['pass_outcome'])
+    s_passes=borussia_match[success]
+    us_passes=borussia_match[~success]
+    s_x_start=s_passes['x_start'].to_list()
+    s_x_end=s_passes['x_end'].to_list()
+    s_y_start=s_passes['y_start'].to_list()
+    s_y_end=s_passes['y_end'].to_list()
+    us_x_start=us_passes['x_start'].to_list()
+    us_x_end=us_passes['x_end'].to_list()
+    us_y_start=us_passes['y_start'].to_list()
+    us_y_end=us_passes['y_end'].to_list()
+
+    pitch.scatter(s_x_start,s_y_start,c='green',ax=ax,label='Origin of successful pass')
+    pitch.scatter(us_x_start,us_y_start,c='red',ax=ax,label='Origin of unsuccessful pass')
+    lc1=pitch.lines(s_x_start, s_y_start, s_x_end, s_y_end, lw=3, comet=True, color='green', ax=ax, label='Successful Passes',transparent=True)
+    lc2=pitch.lines(us_x_start, us_y_start, us_x_end, us_y_end, lw=3, comet=True, color='red', ax=ax, label='Unsuccessful Passes',transparent=True)
+    ax.legend(facecolor='white', edgecolor='black', fontsize=10, loc='upper left', handlelength=7)
+    ax.set_title('Borussia Dortmund: Passes made under Pressure v/s Augsburg, 2015/16', fontsize=18)
+    n='Borussia Dortmund Passes made under Pressure vs Augsburg'
+    fig.savefig(f'{n}.png')
+
+
+
+    return(fig.show)
+
+bor_dor_match=sb.events(match_id=m)
+bor_dor_match_shot_data=bor_dor_match[['team','type','shot_type','shot_technique','location','player','shot_outcome','shot_statsbomb_xg']].reset_index()
+
+shots=bor_dor_match_shot_data[(bor_dor_match_shot_data['team']=='Borussia Dortmund') & (bor_dor_match_shot_data['type']=='Shot') ]
+shots[['x_start', 'y_start']]=pd.DataFrame(shots.location.to_list(), index=shots.index)
+
+def make_shot_plot(shot_data):
+    pitch = VerticalPitch(pad_bottom=0.5,  half=True,  goal_type='box')
+    fig , ax =pitch.draw(figsize=(15,8))
+    penalty=shot_data['shot_outcome']=='Penalty'
+    open_play_shots=shot_data[~penalty]
+    penalty_shots=shot_data[penalty]
+    #['Wayward', 'Blocked', 'Off T', 'Goal', 'Saved']
+    Wayward_shots=shot_data[shot_data['shot_outcome']=='Wayward']
+    Blocked_shots=shot_data[shot_data['shot_outcome']=='Blocked']
+    Off_T_shots=shot_data[shot_data['shot_outcome']=='Off T']
+    Goals=shot_data[shot_data['shot_outcome']=='Goal']
+    Saved_shots=shot_data[shot_data['shot_outcome']=='Saved']
+
+    goals=pitch.scatter(Goals['x_start'], Goals['y_start'],s=((Goals['shot_statsbomb_xg']*500)+100), marker='football',edgecolors='#000000',ax=ax, label='Goals')
+
+    wayward=pitch.scatter(Wayward_shots['x_start'], Wayward_shots['y_start'],s=((Wayward_shots['shot_statsbomb_xg']*500)+100), marker='^',edgecolors='#000000',c='#c70000',ax=ax, label='Wayward shots')
+
+    off_t=pitch.scatter(Off_T_shots['x_start'], Off_T_shots['y_start'],s=((Off_T_shots['shot_statsbomb_xg']*500)+100), marker='X',edgecolors='#000000',c='#006f3c',ax=ax, label='Off Target')
+
+    blocked=pitch.scatter(Blocked_shots['x_start'], Blocked_shots['y_start'],s=((Blocked_shots['shot_statsbomb_xg']*500)+100), marker='h',edgecolors='#000000',c='#f9a73e',ax=ax, label='Blocked')
+
+    saved=pitch.scatter(Saved_shots['x_start'], Saved_shots['y_start'],s=((Saved_shots['shot_statsbomb_xg']*500)+100), marker='o',edgecolors='#000000',c='#264b96',ax=ax, label='Saved')
+
+    ax.legend(facecolor='#4a4e69', edgecolor='white',labelcolor='white', fontsize=20, loc='lower left')
+    ax.set_title('(Greater size refers to greater xG)', fontsize=10)
+    fig.suptitle('Borussia Dortmund: Shots attempted v/s Augsburg, 2015/16', fontsize=18)
+    n='Borussia Dortmund Shots vs Augsburg'
+    fig.savefig(f'{n}.png')
+
+    return(fig.show)
+
+st.pyplot(make_shot_plot(shots))
